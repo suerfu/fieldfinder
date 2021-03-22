632b546d8932d68f46a0e7de3dbba095356109f9
suerfu <suerfu64@gmail.com>
2021-03-22 11:26:42 -0700

    Working version using ROOT's minimization routine instead of histogram fitting. This version only needs the main file.

--- include/FieldInterpolator.h
+++ include/FieldInterpolator.h
@@ -5,64 +5,115 @@
 #include <vector>
 #include <cmath>
 
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+//#include "TError.h"
+#include <iostream>
+
 using namespace std;
 
+//! Abstract class for electric field templates. 
 class ElectricField{
 
 public:
     
-    ElectricField( double a, double b ): centerx(a), centery(b){}
+    //! Constructor. Currently it's using a 2D Gaussian as a placeholder.
+    ElectricField( double a ): center(a){}
 
     ~ElectricField(){}
 
-    double operator()( double* x ){ return -exp( -(x[0]-centerx)*(x[0]-centerx)/2-(x[1]-centery)*(x[1]-centery)/2);}
+    double operator()(){ return center;}
         // For test purpose evaluate a 2D Gaussian.
 
 private:
-
-    double centerx;
-    double centery;
+    
+    double center;
 };
 
 class FieldInterpolator{
 
 public:
 
-    FieldInterpolator( ){ fields.clear();}
+    FieldInterpolator( ){}
         //!< Default constructor.
-
-    FieldInterpolator( vector<ElectricField> f ){ LoadFields(f);}
-        //!< Constructor.
-        //!< In the implementation ElectricField should be changed to actual field object.
 
     ~FieldInterpolator(){}
         //!< Destructor.
+    
+    void LoadField( ElectricField f){ fields.push_back(f);}
+        //!< Add electric field templates.
 
     unsigned int GetDOF(){ return fields.size();}
         //!< Returns the number of field templates to be used.
 
-    void LoadFields( vector<ElectricField> f){ fields = f;}
-
-    //unsigned int GetDimension(){ return 2;}
-        //!< Dimension of the field. 2D for now.
-
     //! Evaluate the interpolated field at location X.
-    double operator()( double* x, double* p){
-        cout << "Evaluating at " << x[0] << ", "<< x[1] << endl;;
+    //! The number of elements pointed to by x should be the same as the number of field templates.
+    //! It is a wrapper for ComputeLikelihood. This wrapper is required for ROOT's minimizer template.
+    double operator()( double* x ){
+        return ComputeLikelihood( x );
+    }
 
-        double f = 0;
-
+    //! Compute the likelihood between observed Kr83 events and the linear combination of electric fields for the given sets of parameters.
+    //! For now this is a mock-up function that returns ND Gaussian whose center is the respective center of each fields.
+    double ComputeLikelihood( double* x){
+        
+        double exponent = 0;
         for( unsigned int i=0; i<fields.size(); i++){
-            f += p[i] * fields[i](x);
+            exponent += -0.5*( x[i]-fields[i]() ) * ( x[i]-fields[i]() );
+        }
+        return exp(exponent);
+    }
+
+    //! Actual function that does the minimization.
+    void Minimize( const char* minName = "Minuit", const char* algoName = "Simplex"){
+        
+        // Initialize ROOT minimizer object
+        ROOT::Math::Minimizer* minimizer = ROOT::Math::Factory::CreateMinimizer(minName, algoName);
+        // Set a number of required parameters
+        minimizer->SetMaxIterations( maxIteration );
+        minimizer->SetTolerance( tolerance )
+        minimizer->SetPrintLevel( printLevel );
+
+        // Create a callable function pointer
+        // Argument is C++ object and dimension/dof.
+        ROOT::Math::Functor f( *this, fields.size() );
+
+        // Set function to minimize.
+        minimizer->SetFunction( f );
+
+        // Initialize variables and their step sizes.
+        for( unsigned int i=0; i<fields.size(); i++ ){
+            stringstream varName;
+            varName << "x" << i;
+            minimizer->SetVariable( i, varName.str().c_str(), variables[i], steps[i]);
         }
 
-        return f;
+        // Do the actual minimization.
+        minimizer->Minimize();
+
+        cout << "Minimum: f( ";
+        const double *xmin = minimizer->X();
+        for( unsigned int i=0; i<fields.size(); i++){
+            cout << xmin[i] << " ";
+        }
+        cout << ") : " << minimizer->MinValue() << endl;
     }
 
 private:
 
     vector<ElectricField> fields;
         //!< Vector object to hold electric field templates.
+
+    vector<double> variables;
+
+    vector<double> steps;
+
+    unsigned int maxIteration;
+
+    float tolerance;
+
+    int printLevel;
 };
 
 
--- test/Gauss2D.cpp
+++ test/Gauss2D.cpp
@@ -1,116 +1,85 @@
-
-#include "FieldFinder.h"
-#include "FieldInterpolator.h"
-
-#include "TApplication.h"
-
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "TRandom2.h"
+#include "TError.h"
 #include <iostream>
-
-using namespace std;
-
-
-vector<ElectricField> fields;
-
-double interpolate( double *x, double *p) {
-    double f = 0;
-    for( unsigned int i=0; i<fields.size(); i++){
-        f += p[i] * fields[i](x);
-    }
-    return f;
-}
-
-
-
+ 
+class RosenBrock
+ {
+public:
+    double operator()(const double *xx){
+   const Double_t x = xx[0];
+   const Double_t y = xx[1];
+   const Double_t tmp1 = y-x*x;
+   const Double_t tmp2 = 1-x;
+   return 100*tmp1*tmp1+tmp2*tmp2;
+   }
+ };
 
 int main(){
-
-    TApplication* app = new TApplication("app",0,0);
-
-    cout << "Initializing field templates...\n";
-
-    fields.push_back( ElectricField(0,0) );
-    fields.push_back( ElectricField(1,0) );
-    fields.push_back( ElectricField(0,1) );
-    fields.push_back( ElectricField(-1,0) );
-    fields.push_back( ElectricField(0,-1) );
-    fields.push_back( ElectricField(1,1) );
-    fields.push_back( ElectricField(-1,-1) );
-    fields.push_back( ElectricField(-1,1) );
-    fields.push_back( ElectricField(1,-1) );
-
-    cout << "Initializing weights...\n";
-    vector<double> p;
-    for( int i=0; i<9; i++){
-        p.push_back( 1 );
-    }
-
-    cout << "Running FieldFinder...\n";
-    //FieldFinder fieldfinder( "Gauss2D.root", "hist2d");
-    //fieldfinder.FindField( &fieldinterpolator, p);
-    TFile file( "Gauss2D.root" );
-    TH2F* prob = (TH2F*) file.Get( "hist2d" );
+    const char *minName = "Minuit";
+    const char *algoName = "Simplex";
+    //const char *algoName = "";
+    int randomSeed = -1;
     
-    TF2* field = new TF2("field", interpolate, -10, 10, -10, 10, fields.size() );
-    field->SetParameters( &p[0]);
-    prob->Fit("field");
-
-    field->Draw("surf5 same");
-    app->Run();
-
+    // create minimizer giving a name and a name (optionally) for the specific
+    // algorithm
+    // possible choices are:
+    //     minName                  algoName
+    // Minuit /Minuit2             Migrad, Simplex,Combined,Scan  (default is Migrad)
+    //  Minuit2                     Fumili2
+    //  Fumili
+    //  GSLMultiMin                ConjugateFR, ConjugatePR, BFGS,
+    //                              BFGS2, SteepestDescent
+    //  GSLMultiFit
+    //   GSLSimAn
+    //   Genetic
+    ROOT::Math::Minimizer* minimum =
+       ROOT::Math::Factory::CreateMinimizer(minName, algoName);
+ 
+    // set tolerance , etc...
+    minimum->SetMaxFunctionCalls(100000000); // for Minuit/Minuit2
+    minimum->SetMaxIterations(10000);  // for GSL
+    minimum->SetTolerance(0.001);
+    minimum->SetPrintLevel(2);
+ 
+    RosenBrock rb;
+    // create function wrapper for minimizer
+    // a IMultiGenFunction type
+    ROOT::Math::Functor f( rb,2);
+    double step[2] = {0.01,0.01};
+    // starting point
+ 
+    double variable[2] = { -1.,1.2};
+    if (randomSeed >= 0) {
+       TRandom2 r(randomSeed);
+       variable[0] = r.Uniform(-20,20);
+       variable[1] = r.Uniform(-20,20);
+    }
+ 
+    minimum->SetFunction(f);
+ 
+    // Set the free variables to be minimized !
+    minimum->SetVariable(0,"x",variable[0], step[0]);
+    minimum->SetVariable(1,"y",variable[1], step[1]);
+ 
+    // do the minimization
+    minimum->Minimize();
+ 
+    const double *xs = minimum->X();
+    std::cout << "Minimum: f(" << xs[0] << "," << xs[1] << "): "
+              << minimum->MinValue()  << std::endl;
+ 
+    // expected minimum is 0
+    if ( minimum->MinValue()  < 1.E-4  && f(xs) < 1.E-4)
+       std::cout << "Minimizer " << minName << " - " << algoName
+                 << "   converged to the right minimum" << std::endl;
+    else {
+       std::cout << "Minimizer " << minName << " - " << algoName
+                 << "   failed to converge !!!" << std::endl;
+       Error("NumericalMinimization","fail to converge");
+    }
+ 
     return 0;
-}
-
-
-/*
-//Fitting 2D hist:
-
-#include "TF2.h"
-#include "TH2.h"
-#include "TMath.h"
-
-// Fitting a 2-D histogram
-// This tutorial illustrates :
-//  - how to create a 2-d function
-//  - fill a 2-d histogram randomly from this function
-//  - fit the histogram
-//  - display the fitted function on top of the histogram
-//
-// This example can be executed via the interpreter or ACLIC
-//   root > .x fit2.C
-//   root > .x fit2.C++
-//Author: Rene Brun
-
-Double_t g2(Double_t *x, Double_t *par) {
-   Double_t r1 = Double_t((x[0]-par[1])/par[2]);
-   Double_t r2 = Double_t((x[1]-par[3])/par[4]);
-   return par[0]*TMath::Exp(-0.5*(r1*r1+r2*r2));
-}
-Double_t fun2(Double_t *x, Double_t *par) {
-   Double_t *p1 = &par[0];
-   Double_t *p2 = &par[5];
-   Double_t *p3 = &par[10];
-   Double_t result = g2(x,p1) + g2(x,p2) + g2(x,p3);
-   return result;
-}
-
-int main() {
-   const Int_t npar = 15;
-   Double_t f2params[npar] =
-      {100,-3,3,-3,3,160,0,0.8,0,0.9,40,4,0.7,4,0.7};
-   TF2 *f2 = new TF2("f2",fun2,-10,10,-10,10, npar);
-   f2->SetParameters(f2params);
-
-   //Create an histogram and fill it randomly with f2
-   TH2F *h2 = new TH2F("h2","from f2",40,-10,10,40,-10,10);
-   Int_t nentries = 100000;
-   h2->FillRandom("f2",nentries);
-   //Fit h2 with original function f2
-   Float_t ratio = 4*nentries/100000;
-   f2params[ 0] *= ratio;
-   f2params[ 5] *= ratio;
-   f2params[10] *= ratio;
-   f2->SetParameters(f2params);
-   h2->Fit("f2");
-   f2->Draw("cont1 same");
-}
-*/
+ }
